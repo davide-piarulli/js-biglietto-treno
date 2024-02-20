22efa3371d1c77ffd56de9b24c59346b534f22cd
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-20 13:58:05 +0000

    calcolo prezzo NO sconto

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const km = prompt('Quanti km dovrai percorrere?');
+console.log(km);
+
+const age = prompt('Quanti anni hai?')
+console.log(age);
+
+const priceKm = 0.21 ;
+
+const ticketPrice = km * priceKm;
+console.log(ticketPrice);
